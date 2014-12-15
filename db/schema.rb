7c0fb1685abc77396be53caa92884526f2a0bee7
henrykerron <henrykerron@gmail.com>
2014-12-15 13:07:48 -0500

    Created Answers Table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215180226) do
+ActiveRecord::Schema.define(version: 20141215180509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: true do |t|
+    t.integer  "question_id", null: false
+    t.integer  "user_id",     null: false
+    t.text     "description", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questions", force: true do |t|
     t.string   "title",       null: false
